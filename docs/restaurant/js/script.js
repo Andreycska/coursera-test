3fbf125163d901d9bbb376fbde9fb59fdd8b3090
Andreycska <pashenkoandrew@gmail.com>
2020-11-19 15:54:41 +0300

    last change

--- docs/restaurant/js/script.js
+++ docs/restaurant/js/script.js
@@ -21,6 +21,9 @@ var homeHtml = "snippets/home-snippet.html";
 var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
+var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+var menuItemsTitleHtml = "snippets/menu-items-title.html";
+var menuItemHtml = "snippets/menu-item.html";
 
 // функция для вставки innerHTML для 'select'
 var insertHtml = function (selector, html) {
@@ -65,6 +68,14 @@ dc.loadMenuCategories = function () {
   $ajaxUtils.sendGetRequest(
     allCategoriesUrl,
     buildAndShowCategoriesHTML);
+};
+
+// Загрузка представление элементов меню 'categoryShort' - это краткое имя для категории
+dc.loadMenuItems = function (categoryShort) {
+  showLoading("#main-content");
+  $ajaxUtils.sendGetRequest(
+    menuItemsUrl + categoryShort,
+    buildAndShowMenuItemsHTML);
 };
 
 
@@ -115,6 +126,124 @@ function buildCategoriesViewHtml(categories,
 
   finalHtml += "</section>";
   return finalHtml;
+}
+
+// Создает HTML для страницы одной категории на основе данных с сервера
+function buildAndShowMenuItemsHTML (categoryMenuItems) {
+  // Загрузка фрагмента заголовка страницы пунктов меню
+  $ajaxUtils.sendGetRequest(
+    menuItemsTitleHtml,
+    function (menuItemsTitleHtml) {
+      // Получить фрагмент отдельного пункта меню
+      $ajaxUtils.sendGetRequest(
+        menuItemHtml,
+        function (menuItemHtml) {
+          var menuItemsViewHtml =
+            buildMenuItemsViewHtml(categoryMenuItems,
+                                   menuItemsTitleHtml,
+                                   menuItemHtml);
+          insertHtml("#main-content", menuItemsViewHtml);
+        },
+        false);
+    },
+    false);
+}
+
+
+// Используя данные категорий и пунктов меню и фрагменты, элементы меню сборки html просматривают HTML-код для вставки на страницу
+function buildMenuItemsViewHtml(categoryMenuItems,
+                                menuItemsTitleHtml,
+                                menuItemHtml) {
+
+  menuItemsTitleHtml =
+    insertProperty(menuItemsTitleHtml,
+                   "name",
+                   categoryMenuItems.category.name);
+  menuItemsTitleHtml =
+    insertProperty(menuItemsTitleHtml,
+                   "special_instructions",
+                   categoryMenuItems.category.special_instructions);
+
+  var finalHtml = menuItemsTitleHtml;
+  finalHtml += "<section class='row'>";
+
+  // Перебирает пункты меню
+  var menuItems = categoryMenuItems.menu_items;
+  var catShortName = categoryMenuItems.category.short_name;
+  for (var i = 0; i < menuItems.length; i++) {
+    // Вставляеи значения пунктов меню
+    var html = menuItemHtml;
+    html =
+      insertProperty(html, "short_name", menuItems[i].short_name);
+    html =
+      insertProperty(html,
+                     "catShortName",
+                     catShortName);
+    html =
+      insertItemPrice(html,
+                      "price_small",
+                      menuItems[i].price_small);
+    html =
+      insertItemPortionName(html,
+                            "small_portion_name",
+                            menuItems[i].small_portion_name);
+    html =
+      insertItemPrice(html,
+                      "price_large",
+                      menuItems[i].price_large);
+    html =
+      insertItemPortionName(html,
+                            "large_portion_name",
+                            menuItems[i].large_portion_name);
+    html =
+      insertProperty(html,
+                     "name",
+                     menuItems[i].name);
+    html =
+      insertProperty(html,
+                     "description",
+                     menuItems[i].description);
+
+    // Добавляйте clearfix после каждого второго пункта меню
+    if (i % 2 != 0) {
+      html += "<div class='row justify-content-between'></div>";
+    }
+
+    finalHtml += html;
+  }
+
+  finalHtml += "</section>";
+  return finalHtml;
+}
+
+
+// Добавляет цену с помощью символа "$", если цена существует.
+function insertItemPrice(html,
+                         pricePropName,
+                         priceValue) {
+  // Если не указано, заменить пустой строкой
+  if (!priceValue) {
+    return insertProperty(html, pricePropName, "");;
+  }
+
+  priceValue = "$" + priceValue.toFixed(2);
+  html = insertProperty(html, pricePropName, priceValue);
+  return html;
+}
+
+
+// Добавляет название части в скобках, если оно существует
+function insertItemPortionName(html,
+                               portionPropName,
+                               portionValue) {
+  // Если не указано, вернуть исходную строку
+  if (!portionValue) {
+    return insertProperty(html, portionPropName, "");
+  }
+
+  portionValue = "(" + portionValue + ")";
+  html = insertProperty(html, portionPropName, portionValue);
+  return html;
 }
 
 
